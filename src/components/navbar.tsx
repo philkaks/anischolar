72abebe0241b78c5076df8e3ac204fbdbfa3d886
Allan Smith <allan@vihlix.com>
2024-09-10 11:16:36 +0300

    done with the responsiveness

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,13 +1,49 @@
 import { Link } from "react-router-dom";
 import logo from "../assets/img/logo1.png";
-import {  useEffect } from "react";
+import { useEffect } from "react";
 import React from "react";
 import { useAuth } from "../authProvider";
 
 const navbar = () => {
-
   const { isLoggedIn, logout } = useAuth();
 
+  useEffect(() => {
+    const navbar = document.querySelector("#navbar") as HTMLElement;
+    const toggleButton = document.querySelector(
+      ".mobile-nav-toggle"
+    ) as HTMLElement;
+
+    const mobileNavToggle = (e: MouseEvent) => {
+      const target = e.target as HTMLElement;
+
+      if (target.matches(".mobile-nav-toggle")) {
+        if (navbar) {
+          navbar.classList.toggle("navbar-mobile");
+        }
+        target.classList.toggle("bi-list");
+        target.classList.toggle("bi-x");
+      } else {
+        const isClickInsideNavbar = navbar.contains(target);
+        const isClickOnToggleButton = toggleButton.contains(target);
+
+        if (
+          !isClickInsideNavbar &&
+          !isClickOnToggleButton &&
+          navbar.classList.contains("navbar-mobile")
+        ) {
+          navbar.classList.remove("navbar-mobile");
+          toggleButton.classList.remove("bi-x");
+          toggleButton.classList.add("bi-list");
+        }
+      }
+    };
+
+    document.addEventListener("click", mobileNavToggle);
+
+    return () => {
+      document.removeEventListener("click", mobileNavToggle);
+    };
+  }, []);
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const scrollto = (el: string) => {
@@ -83,12 +119,12 @@ const navbar = () => {
       <header id="header" className="fixed-top d-flex align-items-center">
         <div className="container d-flex align-items-center justify-content-between">
           <div className="logo">
-            <a href="index.html">
+            <a>
               <img src={logo} alt="" className="img-fluid"></img>
             </a>
             AniScholar
           </div>
-          <nav id="navbar" className="navbar">
+          <nav id="navbar" className="mobile-view navbar">
             <ul>
               <li>
                 <a className="nav-link scrollto" href="#hero">
@@ -126,21 +162,21 @@ const navbar = () => {
                 </a>
               </li>
               {isLoggedIn ? (
-              <li>
-                <button
-                  className="btn text-decoration-none getstarted"
-                  onClick={logout}
-                >
-                  Logout
-                </button>
-              </li>
-            ) : (
-              <li>
-                <Link to="/login" className="text-decoration-none getstarted">
-                  Login
-                </Link>
-              </li>
-            )}
+                <li>
+                  <button
+                    className="btn text-decoration-none getstarted"
+                    onClick={logout}
+                  >
+                    Logout
+                  </button>
+                </li>
+              ) : (
+                <li>
+                  <Link to="/login" className="text-decoration-none getstarted">
+                    Login
+                  </Link>
+                </li>
+              )}
             </ul>
             <i className="bi bi-list mobile-nav-toggle"></i>
           </nav>
