843d1e695138d919adace5cb47b6630842bbd504
Vision-Tey <nimusiimavision@gmail.com>
2024-12-17 05:57:23 +0300

    Make changes to the hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -13,7 +13,7 @@ const Hero = () => {
       <section id="hero">
         <section className="d-flex align-items-center">
           <div className="container">
-            <div className="row">
+            <div className="row items-center">
               <div className="col-lg-5 pt-4 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center hero-left">
                 <h1 className="text-7xl flex flex-col" data-aos="fade-up">
                   Launch 
